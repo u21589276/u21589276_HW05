1c39ffceb8a310b73a6c8491794c0599b9a70298
mila0523 <102968933+u21589276@users.noreply.github.com>
2022-09-22 15:54:03 +0200

    getallbooks methods done

--- Super-Duper Library/Models/ServiceData.cs
+++ Super-Duper Library/Models/ServiceData.cs
@@ -11,14 +11,46 @@ namespace Super_Duper_Library.Models
     public class ServiceData
     {
         private String ConnectionString;
-        SqlConnection myconnection = new SqlConnection("Data Source=cmsa\tew_sqlexpress;Initial Catalog=Library;Integrated Security=True");
+        SqlConnection myConnection = new SqlConnection("Data Source=cmsa\tew_sqlexpress;Initial Catalog=Library;Integrated Security=True");
 
         public ServiceData()
         {
 
         }
 
+        public List<Books> getAllBooks()
+        {
+            List<Books> bookData = new List<Books>();
+            //TODO: Complete this
+            try
+            {
+                myConnection.Open();
+                SqlCommand getbookCommand = new SqlCommand("select * from books", myConnection);
+                SqlDataReader myReader = getbookCommand.ExecuteReader();
+                while (myReader.Read())
+                {
+                    Books books = new Books();
+                    books.BookId = Convert.ToInt32(myReader["bookId"]);
+                    books.Name = myReader["name"].ToString();
+                    books.Pagecount = Convert.ToInt32(myReader["pagecount"]);
+                    books.Point = Convert.ToInt32(myReader["point"]);
+                    books.AuthorId = Convert.ToInt32(myReader["authorId"]);
+                    books.TypeId = Convert.ToInt32(myReader["typeId"]);
 
+
+                    bookData.Add(books);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return bookData;
+        }
 
     }
 }
